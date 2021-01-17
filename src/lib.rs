1cbec75cdb2393dc2db93f53e0d1e74485b40a89
Jason Kim <dialogbox@gmail.com>
2021-01-17 23:17:43 +0900

    basic implementation

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,10 @@ pub struct MandelImage {
     width: usize,
     height: usize,
 
+    upper_left: (f64, f64),
+    lower_right: (f64, f64),
+    limit: u32,
+
     pixels: Vec<[u8; 4]>,
 }
 
@@ -34,15 +38,92 @@ impl MandelImage {
         MandelImage {
             width,
             height,
+            upper_left: (-2.0, 1.5),
+            lower_right: (1.0, -1.5),
+            limit: 2,
             pixels: vec![[0, 0, 0, 255]; width * height],
         }
     }
 
-    fn get_index(&self, x: usize, y: usize) -> usize {
-        (y * self.width + x) as usize
+    // fn get_index(&self, x: usize, y: usize) -> usize {
+    //     (y * self.width + x) as usize
+    // }
+
+    fn index_to_point(&self, i: usize) -> (f64, f64) {
+        let y = i / self.width;
+        let x = i % self.width;
+
+        self.pixel_to_point((x, y))
     }
 
-    fn draw_mandel(&self) {}
+    pub fn draw_mandel(&mut self, limit: u32) {
+        self.limit = limit;
+        for i in 0..self.pixels.len() {
+            let result = self.is_in_mandel(self.index_to_point(i));
+            Self::get_color(result, self.limit, &mut self.pixels[i]);
+        }
+    }
+
+    /// Given the row and column of a pixel in the output image, return the
+    /// corresponding point on the complex plane.
+    ///
+    /// `bounds` is a pair giving the width and height of the image in pixels.
+    /// `pixel` is a (column, row) pair indicating a particular pixel in that image.
+    /// The `upper_left` and `lower_right` parameters are points on the complex
+    /// plane designating the area our image covers.
+    fn pixel_to_point(&self, pixel: (usize, usize)) -> (f64, f64) {
+        let (width, height) = (
+            self.lower_right.0 - self.upper_left.0,
+            self.upper_left.1 - self.lower_right.1,
+        );
+        (
+            self.upper_left.0 + pixel.0 as f64 * width / self.width as f64,
+            self.upper_left.1 - pixel.1 as f64 * height / self.height as f64,
+            // Why subtraction here? pixel.1 increases as we go down,
+            // but the imaginary component increases as we go up.
+        )
+    }
+
+    /// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
+    /// iterations to decide.
+    ///
+    /// If `c` is not a member, return `Some(i)`, where `i` is the number of
+    /// iterations it took for `c` to leave the circle of radius two centered on the
+    /// origin. If `c` seems to be a member (more precisely, if we reached the
+    /// iteration limit without being able to prove that `c` is not a member),
+    /// return `None`.
+    fn is_in_mandel(&self, c: (f64, f64)) -> Option<u32> {
+        let mut z = (0.0, 0.0);
+        for i in 0..self.limit {
+            z = (
+                (z.0 * z.0) - (z.1 * z.1) + c.0,
+                (z.0 * z.1) + (z.1 * z.0) + c.1,
+            );
+            let norm_sqr = (z.0 * z.0) + (z.1 * z.1);
+            if norm_sqr > 4.0 {
+                return Some(i);
+            }
+        }
+
+        None
+    }
+
+    fn get_color(num: Option<u32>, limit: u32, buf: &mut [u8; 4]) {
+        buf[3] = 255;
+        match num {
+            None => {
+                buf[0] = 0;
+                buf[1] = 0;
+                buf[2] = 0
+            }
+            Some(i) => {
+                let i = (i as f64 / (limit as f64 / 255.0)) as u8;
+                buf[0] = i;
+                buf[1] = i;
+                buf[2] = i;
+            }
+        }
+    }
 }
 
 impl Default for MandelImage {
@@ -52,7 +133,10 @@ impl Default for MandelImage {
         MandelImage {
             width,
             height,
-            pixels: vec![[0, 0, 0, 0]; width * height],
+            upper_left: (-2.0, 2.0),
+            lower_right: (2.0, -2.0),
+            limit: 255,
+            pixels: vec![[0, 0, 0, 255]; width * height],
         }
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,7 +1,7 @@
 import { MandelImage } from "wasm-mandel";
 import { memory } from "wasm-mandel/wasm_mandel_bg";
 
-const mandel_image = MandelImage.new(1024, 1024);
+const mandel_image = MandelImage.new(1280, 1280);
 const width = mandel_image.width();
 const height = mandel_image.height();
 
@@ -15,12 +15,17 @@ const pixelsPtr = mandel_image.pixels();
 var usub = new Uint8ClampedArray(memory.buffer, pixelsPtr, height * height * 4);
 var img = new ImageData(usub, width, height);
 
-ctx.putImageData(img, 0, 0);
 
+const fps = 5;
+var limit = 1;
 // We don't need this unless doing animation
-// function step() {
-    // ctx.putImageData(img, 0, 0)
-        // window.requestAnimationFrame(step);
-// }
-// 
-// window.requestAnimationFrame(step);
+function step() {
+    setTimeout(function () {
+        mandel_image.draw_mandel(limit);
+        ctx.putImageData(img, 0, 0)
+        limit = limit + 1;
+        window.requestAnimationFrame(step);
+    }, 1000 / fps);
+}
+
+window.requestAnimationFrame(step);
