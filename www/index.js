a49333b240206740eaf5b21b1a30a1d55db7c0e4
Jason Kim <dialogbox@gmail.com>
2021-01-28 12:48:43 +0900

    Add some document and web page improvements

--- www/index.js
+++ www/index.js
@@ -13,26 +13,75 @@ const pixelsPtr = mandel_image.pixels();
 var usub = new Uint8ClampedArray(memory.buffer, pixelsPtr, canvas.width * canvas.height * 4);
 var img = new ImageData(usub, canvas.width, canvas.height);
 
-// Rendering loop
-const MAX_LIMIT = 100;
-const max_fps = 10;
-var limit = 1;
-var limit_steps = 10;
-function step() {
-    setTimeout(function () {
-        fps.render();
+const fps = create_fps_panel();
+draw_mandel(0);
 
-        mandel_image.set_limit(limit);
-        mandel_image.draw_mandel();
-        ctx.putImageData(img, 0, 0)
+let animationId = null;
+
+const steps_input = document.getElementById("steps-input");
+const fps_input = document.getElementById("fps-input");
+
+const MAX_LIMIT = 100;
+var limit = 0;
+var run = false;
+
+const playPauseButton = document.getElementById("play-pause");
+const resetButton = document.getElementById("reset");
+
+playPauseButton.addEventListener("click", event => {
+    if (!run) {
+        play();
+    } else {
+        pause();
+    }
+});
+
+resetButton.addEventListener("click", event => {
+    limit = 0;
+    draw_mandel(0);
+});
+
+function draw_mandel(limit) {
+    fps.render();
+
+    mandel_image.set_limit(limit);
+    mandel_image.draw_mandel();
+    ctx.putImageData(img, 0, 0);
+}
+
+// Rendering loop
+function renderLoop() {
+    if (!run) return;
+
+    const max_fps = parseInt(fps_input.value);
+    const limit_steps = parseInt(steps_input.value);
+
+    console.log(limit, limit_steps, limit+limit_steps, max_fps);
+    setTimeout(function () {
+        draw_mandel(limit);
         limit = Math.min(limit + limit_steps, MAX_LIMIT);
         if (limit < MAX_LIMIT) {
-            window.requestAnimationFrame(step);
+            animationId = requestAnimationFrame(renderLoop);
         }
     }, 1000 / max_fps);
 }
 
-window.requestAnimationFrame(step);
+function play() {
+    console.log("Play");
+    run = true;
+    // playPauseButton.textContent = "Stop";
+    playPauseButton.setAttribute("value", "Stop");
+    renderLoop();
+}
+
+function pause() {
+    console.log("Pause");
+    run = false;
+    // playPauseButton.textContent = "Start";
+    playPauseButton.setAttribute("value", "Start");
+    cancelAnimationFrame(animationId);
+    animationId = null;
+}
 
 function setup_canvas(canvas, width, height, dpr = (window.devicePixelRatio || 1)) {
     const ctx = canvas.getContext('2d');
@@ -51,7 +100,8 @@ function setup_canvas(canvas, width, height, dpr = (window.devicePixelRatio || 1
     return ctx;
 }
 
-const fps = new class {
+function create_fps_panel() {
+return new class {
     constructor() {
         this.fps = document.getElementById("fps");
         this.frames = [];
@@ -93,3 +143,5 @@ const fps = new class {
   `.trim();
     }
 };
+
+}
